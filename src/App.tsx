7f3a318c641074978ecd1c8ab3bcf7d09c6057a5
irfan_usuf <irfanusuf33@gmail.com>
2025-01-09 15:49:10 +0530

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,28 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ResponsiveAppBar from "./components/shared/AppBar";
 import Button from "./components/atoms/Button";
 import Home from "./components/pages/Home";
-
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { UserDataRequest, UserRegister } from "./redux/actions";
+import { AppDispatch } from "./redux/store";
 
 function App() {
+  const dispatch: AppDispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(UserDataRequest());
+  }, []);
+
+  const formData: { [index: string]: string } = {
+    username: "meowwo",
+    email: "mewowo@gmail.com",
+    Password: "1232234",
+  };
+
+  const handleClick = () => {
+    dispatch(UserRegister(formData));
+  };
+
   return (
     <>
       <BrowserRouter>
@@ -12,16 +31,14 @@ function App() {
 
         <Button
           name="hello"
-          clickHandler={() => {}}
+          clickHandler={handleClick}
           color="red"
           bgColor="blue"
         />
 
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Home />} />
         </Routes>
-
-        
       </BrowserRouter>
     </>
   );
